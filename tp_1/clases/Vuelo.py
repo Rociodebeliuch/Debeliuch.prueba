e7ec4c3ac6ca7eb49fd7ffa24553d9013e1028c2
Rocio <rochidebeliuch@gmail.com>
2017-06-02 11:46:43 -0300

    aa

--- tp_1/clases/Vuelo.py
+++ tp_1/clases/Vuelo.py
@@ -9,8 +9,6 @@ class Vuelos(object):
 
     def __init__(self):
         self.l_pasajeros=[]
-
-    def __init__(self):
         self.l_tripulacion=[]
 
     def Agregaravion(self,av):
@@ -33,3 +31,45 @@ class Vuelos(object):
 
     def AgregarT(self,t):
         self.l_tripulacion.append(t)
+
+    def Personas_por_vuelo(self):
+        lista_aux = []
+        for item in self.l_tripulacion:
+            lista_aux.append(item)
+
+        for item2 in self.l_pasajeros:
+            lista_aux.append(item2)
+
+        return lista_aux
+
+    def Persona_mas_joven(self):
+        aux = self.l_pasajeros[1].fecha_nac
+        persona_aux = None
+        for item in self.l_pasajeros:
+            if item.fecha_nac > aux:
+                aux = item.fecha_nac
+                persona_aux = item
+        return persona_aux
+
+    def NoseAlcanza(self):
+        cant = True
+        if len(self.l_tripulacion) < self.avion.cant_trip:
+            cant = False
+        return cant
+
+    def EstaAutorizado(self):
+        aux = None
+        for item in self.l_tripulacion:
+            for item2 in item.modelos_avion:
+                if item2 == self.avion.codunico:
+                    aux = True
+                aux = False
+            return aux
+
+    def PersonasVipoNec(self):
+        for item in self.l_pasajeros:
+            if item.vip == True:
+                return item
+            if item.n_especiales != None:
+                return item
+
--- tp_1/programa.py
+++ tp_1/programa.py
@@ -15,10 +15,10 @@ def AgregarAviones():
     f = open('/home/alumno/Descargas/aviones.dat', 'r')
     for line in f:
         un_avion = Aviones()
-        lista = line.split('|')
-        un_avion.setcod(lista[0])
-        un_avion.setAgregarCantP(lista[1])
-        un_avion.setAgregarCantT(lista[2])
+        l = line.split('|')
+        un_avion.setcod(l[0])
+        un_avion.setAgregarCantP(l[1])
+        un_avion.setAgregarCantT(l[2])
         listaAv.append(un_avion)
 
     f.close()
@@ -26,7 +26,6 @@ def AgregarAviones():
 def AgregarPersonas():
     f=open('/home/alumno/Descargas/personas.dat', 'r')
     for line in f:
-        una_persona=Personas()
         l=line.split('|')
         if l[0]=="Pasajero":
             un_pasajero=Pasajeros()
@@ -34,53 +33,42 @@ def AgregarPersonas():
             un_pasajero.setAgregarApellido(l[2])
             un_pasajero.setAgregarFechaN(l[3])
             un_pasajero.setAgregarDni(l[4])
-            if l[5]==0:
-                un_pasajero.vip(None)
-            else:
-                 un_pasajero.vip(l[5])
+            un_pasajero.vip(l[5])
             if len(l)==6:
                 un_pasajero.NecesidadesEsp(l[6])
             else:
                 un_pasajero.NecesidadesEsp(None)
             listaPer.append(un_pasajero)
 
-        if l[0]=="Piloto":
+        elif l[0]=="Piloto":
             un_piloto=Piloto()
             un_piloto.setAgregarNombre(l[1])
             un_piloto.setAgregarApellido(l[2])
             un_piloto.setAgregarFechaN(l[3])
             un_piloto.setAgregarDni(l[4])
-            if (l)==5:
-                x=l.split(',')
-                largo=len(x)
-                while largo <0:
-                    for avion in listaAv:
-                        if avion.codigo==x[largo]:
+            x=l[5].split(',')
+            for item in x:
+                  for avion in listaAv:
+                        if avion.codigo==item:
                             un_piloto.AgregarModelo(avion)
-                    largo=largo-1
+
 
             listaPer.append(un_piloto)
 
-        if l[0]=="Servicio":
+        elif l[0]=="Servicio":
             un_servicio=Servicio()
             un_servicio.setAgregarNombre(l[1])
             un_servicio.setAgregarApellido(l[2])
             un_servicio.setAgregarFechaN(l[3])
             un_servicio.setAgregarDni(l[4])
-            if (l)==5:
-                x=l.split(',')
-                largo=len(x)
-                while largo<0:
-                    for item in listaAv:
-                        if item.codigo==x[largo]:
-                            un_servicio.AgregarModelo(item)
-                largo=largo-1
-            if(l)==6:
-                y=l.split(',')
-                long=len(y)
-                while long<0:
-                    un_servicio.AgregarIdiom(y[long])
-                    long=long-1
+            x=l[5].split(',')
+            for item in x:
+                for avion in listaAv:
+                    if avion.codigo == item:
+                        un_servicio.AgregarModelo(avion)
+            y=l[6].split(',')
+            for item in y:
+                un_servicio.AgregarIdiom(item)
             listaPer.append(un_servicio)
 
 
@@ -98,29 +86,34 @@ def AgregarVuelos():
         un_vuelo.AgregarHora(l[2])
         un_vuelo.AgregarOrigen(l[3])
         un_vuelo.AgregarDestino(l[4])
-        if (l)==5:
-            x=l.split(',')
-            long=len(x)
-            while long<0:
-                for tripulante in listaPer:
-                    if tripulante.dni==x[long]:
-                        un_vuelo.AgregarT(tripulante)
-                long=long-1
-        if(l)==6:
-            y=l.split(',')
-            largo=len(y)
-            while largo<0:
-                for pasajero in listaPer:
-                    if pasajero.dni==y[largo]:
-                         un_vuelo.AgregarP(pasajero)
-                largo=largo-1
+        x=l[5].split(',')
+        for item in x:
+            for persona in listaPer:
+                if persona.dni==item:
+                    un_vuelo.AgregarT(persona)
+        y=l[6].split(',')
+        for item in y:
+            for pasajero in listaPer:
+                if pasajero.dni==item:
+                    un_vuelo.AgregarP(pasajero)
 
         listaVuelos.append(un_vuelo)
 
     f.close()
 
+def EstaAutorizado():
+    vuela=True
+    listaaux=[]
+    for vuelo in listaVuelos:
+        vuela=vuelo.EstaAutorizado
+        if vuela==False:
+            listaaux.append(vuelo)
+    return listaaux
+
 
 def TripViajaMas():
+    x=None
+    y=None
     for vuelo in listaVuelos:
         x=vuelo.fecha
         for trip in vuelo.l_tripulantes:
@@ -132,6 +125,8 @@ def TripViajaMas():
                                     return item2
 
 
+
+
 def mostrarListaAv():
     AgregarAviones()
     for item in listaAv:
@@ -140,17 +135,25 @@ def mostrarListaAv():
         print (item.cant_trip)
 
 
-mostrarListaAv()
+
 
 def mostrarListaPersonas():
     AgregarPersonas()
     for item in listaPer:
-        print(item.nombre)
-        print(item.apellido)
-        print(item.fecha_nac)
-        print(item.dni)
+        if type(item) is Piloto:
+            print(item.nombre)
+            print(item.apellido)
+            print(item.fecha_nac)
+            print(item.dni)
+            for item2 in item.modelos_avion:
+                print(item2.codigo)
 
-mostrarListaPersonas()
+
+
+
+
+
+
 
 def mostrarListaVuelos():
     AgregarVuelos()
@@ -166,7 +169,18 @@ def mostrarListaVuelos():
         for item3 in item.l_tripulacion:
             print(item3.nombre)
 
-mostrarListaVuelos()
+
+
+
+def mostrar():
+    AgregarPersonas()
+    listax=[]
+    for item in listaVuelos:
+        listax=item.Personas_por_vuelo(item)
+        for per  in listax:
+            print(per.nombre)
+
+mostrar()
 
 
 
